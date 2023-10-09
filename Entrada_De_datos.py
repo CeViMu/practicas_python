b0d75bb6f65cd1a03eaeccbefa8b0473a481b4ce
C M <micho071115@hotmail.com>
2023-10-08 18:40:23 -0600

    Modificacion

--- Entrada_De_datos.py
+++ Entrada_De_datos.py
@@ -1,5 +1,5 @@
-nombre = input("¿Cuál es tu nombre?")
-print("Hola" + nombre + ", vamos a realizar una suma" )
+nombre = input("¿Cuál es tu nombre? ")
+print("Hola " + nombre + ", vamos a realizar una suma" )
 
 num_1 = int(input("Ingresa el primer valor: "))
 num_2 = int(input("Ingresa el segundo valor: "))
--- condicionales.py
+++ condicionales.py
@@ -1,5 +1,15 @@
 print("Sistema para calcular el promedio de un alumno")
 
 nombre= input("Escribe tu nombre: ")
-matematicas=int(input(nombre + ", ¿Cuál es tu calificación en matemáticas?"))
-quimica=int(input(nombre + ", ¿Cuál es tu calificación en química?"))
+matematicas=int(input(nombre + ", ¿Cuál es tu calificación en matemáticas? "))
+quimica=int(input(nombre + ", ¿Cuál es tu calificación en química? "))
+biologia = int(input(nombre + " ¿Cuál es tu calificación en biología? "))
+
+promedio = (matematicas + quimica + biologia)/3
+promedio_entero = int(promedio)
+#No se puede concatenar de manera directa un string con un number
+#podemos usar una coma
+if promedio >= 6 :
+    print('Felicidades ' + nombre + '" aprobaste" con un promedio de ', promedio)
+    
+print("Fin")
