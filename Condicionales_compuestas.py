739357721a63dc9139cf56a207a7fdd5bef648a7
C M <micho071115@hotmail.com>
2023-11-04 19:10:38 -0600

    Agregando ejercicio elif

--- Condicionales_compuestas.py
+++ Condicionales_compuestas.py
@@ -14,3 +14,21 @@ if promedio >= 6:
     print('Felicidades ' + nombre + ' "aprobaste" con un promedio de ', round(promedio, 2));
 else:
     print('Lo sentimos ' + nombre + ' has "reprobado" con un promedio de ', promedio)
+
+
+#elseif
+print("¡Convertidor de números a letras");
+num_uno = int(input("¿Cuál es el número que deseas convertir? "));
+
+if num_uno == 1: 
+    print("El número es uno")
+elif num_uno ==2:
+    print("El número es dos")
+elif num_uno == 3:
+    print("El número es tres")
+elif num_uno == 4:
+    print("El número es cuatro")
+elif num_uno == 5:
+    print("El número es cinco")
+else:
+    print("Este programa solo puede convertir hasta el número cinco")
